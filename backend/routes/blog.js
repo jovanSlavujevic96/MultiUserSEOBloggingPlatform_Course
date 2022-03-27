6623c03bb2d63851d82074767ce6bcc07b525c66
Jovan Slavujevic <jotzilla96@gmail.com>
2022-03-27 12:15:07 +0200

    Controllers

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const {time} = require('../controllers/blog');
 
-router.get('/', (req, res) => {
-    // fixes browser-to-browser communication
-    res.json({time: Date().toString()})
-});
+router.get('/', time);
 
 // any other router created later will be exported from here using `module.exports`
 module.exports = router;
