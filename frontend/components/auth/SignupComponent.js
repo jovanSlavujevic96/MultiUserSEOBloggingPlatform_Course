1209be1fcc3dac714c76b322b8dd7f51bdf3873e
Jovan Slavujevic <jotzilla96@gmail.com>
2022-03-27 21:48:42 +0200

    Signup form pt.1

---
+++ frontend/components/auth/SignupComponent.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {useState} from "react";
+
+const SignupComponent = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('handle submit');
+    };
+
+    const handleChange = e => {
+        console.log(e.target.value);
+    };
+
+    const signupForm = () => {
+        return (
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <input
+                        onChange={handleChange}
+                        type="text"
+                        className="form-control"
+                        placeholder="Type your name"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input
+                        onChange={handleChange}
+                        type="email"
+                        className="form-control"
+                        placeholder="Type your email"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input
+                        onChange={handleChange}
+                        type="password"
+                        className="form-control"
+                        placeholder="Type your password"
+                    />
+                </div>
+
+                <div>
+                    <button className="btn btn-primary">Signup</button>
+                </div>
+            </form>
+        );
+    };
+
+    return (
+        <React.Fragment>
+            {signupForm()}
+        </React.Fragment>
+    );
+};
+
+export default SignupComponent;
