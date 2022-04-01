6cd0e7dc1231cf402b47505d9b64d3e437772ef4
Jovan Slavujevic <jotzilla96@gmail.com>
2022-04-01 23:27:58 +0200

    Category -> list, read & delete

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,6 +1,7 @@
 const Category = require('../models/category');
 const slugify = require('slugify');
 const {errorHandler} = require('../helpers/dbErrorHandler');
+const category = require('../models/category');
 
 exports.create = (req, res) => {
     const {name} = req.body;
@@ -17,3 +18,43 @@ exports.create = (req, res) => {
         res.json(data);
     });
 };
+
+exports.list = (req, res) => {
+    // pass the empty object {} fo find method to get all objects (categories)
+    Category.find({}).exec((err,data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(data);
+    });
+};
+
+exports.read = (req, res) => {
+    const slug = req.params.slug.toLowerCase();
+
+    Category.findOne({slug}).exec((err, category) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(category);
+    });
+};
+
+exports.remove = (req, res) => {
+    const slug = req.params.slug.toLowerCase();
+
+    Category.findOneAndRemove({slug}).exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            message: 'Category deleted successfully'
+        });
+    });
+}
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {create} = require('../controllers/category');
+const {create, list, read, remove} = require('../controllers/category');
 
 // validators
 const {runValidation} = require('../validators');
@@ -8,6 +8,9 @@ const {categoryCreateValidator } = require('../validators/category');
 const {requireSignin, adminMiddleware} = require('../controllers/auth');
 
 router.get('/category', categoryCreateValidator, runValidation, requireSignin, adminMiddleware, create);
+router.get('/categories', list);
+router.get('/category/:slug', read);
+router.delete('/category/:slug', requireSignin, adminMiddleware, remove);
 
 // any other router created later will be exported from here using `module.exports`
 module.exports = router;
