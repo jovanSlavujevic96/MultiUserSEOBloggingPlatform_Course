fe3397c65d1fe2d3658f8eecb43ba8c79142d46a
Jovan Slavujevic <jotzilla96@gmail.com>
2022-04-27 22:11:42 +0200

    Show Single Blog

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import {withRouter} from 'next/router';
+import { withRouter} from 'next/router';
 import Layout from '../../components/Layout';
-import React, { userState } from 'react';
+import React, { useState } from 'react';
 import { listBlogsWithCategoriesAndTags } from '../../actions/blog';
 import Card from '../../components/blog/Card';
-import {API, DOMAIN, APP_NAME, FB_APP_ID} from '../../config';
+import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../../config';
 
-const Blogs = ({ blogs, categories, tags, size, router }) => {
+const Blogs = ({ blogs, categories, tags, totalBlogs, blogsLimit, blogSkip, router }) => {
     const head = () => {
         return <Head>
             <title>Programming blogs | {APP_NAME}</title>
@@ -31,6 +31,37 @@ const Blogs = ({ blogs, categories, tags, size, router }) => {
             <meta property="fb:app_id" content={`${FB_APP_ID}`}/>
         </Head>
     };
+
+    const [limit, setLimit] = useState(blogsLimit);
+    const [skip, setSkip] = useState(blogSkip);
+    const [size, setSize] = useState(totalBlogs);
+    const [loadedBlogs, setLoadedBlogs] = useState([]); /* henever user pressed the button to load more,
+                                                         * then those blocks will be added here */
+
+    const loadMore = () => {
+        let toSkip = skip + limit;
+        listBlogsWithCategoriesAndTags(toSkip, limit).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            }
+            else {
+                setLoadedBlogs([...loadedBlogs, ...data.blogs]);
+                setSize(data.size);
+                setSkip(toSkip);
+            }
+        });
+    }
+
+    const loadMoreButton = () => {
+        return (
+            size > 0 &&
+            size >= limit && (
+                <button onClick={loadMore} className="btn btn-outline-primary btn-lg">
+                    Load more
+                </button>
+            )
+        );
+    }
 
     const showAllBlogs = () => {
         return blogs.map((blog, i) => {
@@ -58,6 +89,14 @@ const Blogs = ({ blogs, categories, tags, size, router }) => {
         ));
     };
 
+    const showLoadedBlogs = () => {
+        return loadedBlogs.map((blog, i) => (
+            <article key={i}>
+                <Card blog={blog}/>
+            </article>
+        ));
+    }
+
     return (
         <React.Fragment>
             {head()}
@@ -78,11 +117,9 @@ const Blogs = ({ blogs, categories, tags, size, router }) => {
                             </section>
                         </header>
                     </div>
-                    <div className='container-fluid'>
-                        <div className="row">
-                            <div className="col-md-12">{showAllBlogs()}</div>
-                        </div>
-                    </div>
+                    <div className='container-fluid'>{showAllBlogs()}</div>
+                    <div className='container-fluid'>{showLoadedBlogs()}</div>
+                    <div className='text-center pt-5 pb-5'>{loadMoreButton()}</div>
                 </main>
             </Layout>
         </React.Fragment>
@@ -95,7 +132,10 @@ Blogs.getInitialProps = () => {
     // and then get the data and then return the data
     // !! IMPORTANT -> return DATA
 
-    return listBlogsWithCategoriesAndTags().then(data => {
+    let skip = 0;
+    let limit = 2;
+
+    return listBlogsWithCategoriesAndTags(skip, limit).then(data => {
         if (data.error) {
             console.log(data.error);
         }
@@ -104,7 +144,9 @@ Blogs.getInitialProps = () => {
                 blogs: data.blogs,
                 categories: data.categories,
                 tags: data.tags,
-                size: data.size,
+                totalBlogs: data.size,
+                blogsLimit: limit,
+                blogSkip: skip
             };
         }
     });
