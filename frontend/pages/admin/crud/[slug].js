978b5e45c4d16bb6726ca3cd9fe662a57d27eb0d
Jovan Slavujevic <jotzilla96@gmail.com>
2022-04-29 11:12:19 +0200

    Update blog - Populate content in rich text editor

--- frontend/pages/admin/crud/[slug].js
+++ frontend/pages/admin/crud/[slug].js
@@ -6,12 +6,7 @@ import BlogUpdate from '../../../components/crud/BlogUpdate';
 import Category from '../../../components/crud/Category';
 import Tag from '../../../components/crud/Tag';
 
-
 const Blog = () => {
-    // <Link> is repalcement for <a> HTML tag
-    // it avoids loading of other page
-    // it just replaces components
-    // but it keeps <a> within
     return (
         <Layout>
             <Admin>
