c0f02c468542b2de58c9719ff70210ea468013de
Jovan Slavujevic <jotzilla96@gmail.com>
2022-04-30 16:45:14 +0200

    User public profile @backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,7 @@
-
 import User from '../models/user.js';
+import Blog from '../models/blog.js';
+import { errorHandler } from '../helpers/dbErrorHandler.js';
+
 
 const read = (req, res) => {
     // we don't want to provide user information with password
@@ -8,3 +10,34 @@ const read = (req, res) => {
 };
 
 export { read };
+
+export const publicProfile = (req, res) => {
+    const username = req.params.username;
+
+    User.findOne({username}).exec((err, userFromDB) => {
+        let user;
+        if (err || !userFromDB) {
+            return res.status(400).json({
+                error: 'User not found'
+            });
+        }
+        user = userFromDB;
+        Blog.find({postedBy: user._id})
+            .populate('categories', '_id name slug')
+            .populate('tags', '_id name slug')
+            .populate('postedBy', '_id name')
+            .limit(10)
+            .select('_id title slug excerpt categories tags postedBy createdAt updatedAd')
+            .exec((err, data) => {
+                if (err) {
+                    return res.status(400).json({
+                        error: errorHandler(err)
+                    });
+                }
+                user.photo = undefined; // just to be sure
+                res.json({
+                    user, blogs: data
+                });
+            });
+    });
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,9 +2,10 @@ import express from 'express';
 const router = express.Router();
 
 import {requireSignin, authMiddleware} from '../controllers/auth.js';
-import {read} from '../controllers/user.js';
+import {read, publicProfile} from '../controllers/user.js';
 
 router.get('/profile', requireSignin, authMiddleware, read);
+router.get('/user/:username', publicProfile, authMiddleware, read);
 
 // any other router created later will be exported from here using `module.exports`
 export default router;
