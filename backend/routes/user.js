a7a1c592e05eba6d666169ea2d6a2f06a373f06c
Jovan Slavujevic <jotzilla96@gmail.com>
2022-05-02 12:34:21 +0200

    Profile update component setup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,8 +4,8 @@ const router = express.Router();
 import {requireSignin, authMiddleware} from '../controllers/auth.js';
 import {read, publicProfile, update, photo} from '../controllers/user.js';
 
-router.get('/profile', requireSignin, authMiddleware, read);
-router.get('/user/:username', publicProfile, authMiddleware, read);
+router.get('/user/profile', requireSignin, authMiddleware, read);
+router.get('/user/:username', publicProfile);
 router.put('/user/update', requireSignin, authMiddleware, update);
 router.get('/user/photo/:username', photo);
 
--- frontend/components/auth/ProfileUpdate.js
+++ frontend/components/auth/ProfileUpdate.js
@@ -1,5 +1,59 @@
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import Router from 'next/router';
+import { getCookie, isAuth } from '../../actions/auth';
+import { getProfile, updateProfile } from '../../actions/user';
+import { initScriptLoader } from 'next/script';
+
 const ProfileUpdate = () => {
-    return <h2>profile update</h2>
+    const [values, setValues] = useState({
+        username: '',
+        name: '',
+        email: '',
+        password: '',
+        error: false,
+        success: false,
+        loading: false,
+        photo: '',
+        userData: typeof window !== 'undefined' && new FormData(),
+    });
+
+    const token = getCookie('token');
+    const {username, name, email, password, error, success, loading, photo, userData} = values;
+
+    const init = () => {
+        getProfile(token).then(data => {
+            if (data.error) {
+                setValues({...values, error: data.error});
+            } else {
+                setValues({...values,
+                    username: data.username,
+                    name: data.name,
+                    email: data.email,
+                });
+            }
+        });
+    };
+
+    useEffect(() => {
+        init();
+    }, []);
+
+    return (
+        <React.Fragment>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-4'>
+                        image
+                    </div>
+                    <div className='col-md-8'>
+                        update form
+                        {JSON.stringify({username, email, name})}
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default ProfileUpdate;
